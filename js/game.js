9ebe61a4a514ea29e8bffc6c300bd3380b6de7ab
KidBourbon <kidbourbongamer@gmail.com>
2023-08-31 04:30:28 -0400

    Implemented player movement. Implemented method to get a map. Implemented method to get the position in canvas given a column and a row.

--- js/game.js
+++ js/game.js
@@ -9,62 +9,104 @@ const btnDown = document.querySelector('#down-button');
 
 let canvasSize = 0;
 let elementSize = 0;
-
 let currentLevel = 1;
+
+// Player position (column and row)
+const playerPos = {
+	col: undefined,
+	row: undefined,
+};
 
 window.addEventListener('load', startGame);
 window.addEventListener('resize', () => {
 	resizeAll();
 	renderMap(currentLevel);
 });
-
-btnUp.addEventListener('click', () => movePlayer(0, 1));
-btnLeft.addEventListener('click', () => movePlayer(-1, 0));
-btnRight.addEventListener('click', () => movePlayer(1, 0));
-btnDown.addEventListener('click', () => movePlayer(0, -1));
-
 window.addEventListener('keydown', (event) => {
 	console.log(event);
 	movePlayerByKey(event.code);
 });
 
-function movePlayer(x, y) {
-	console.log({ x, y });
+btnUp.addEventListener('click', () => movePlayer(0, -1));
+btnLeft.addEventListener('click', () => movePlayer(-1, 0));
+btnRight.addEventListener('click', () => movePlayer(1, 0));
+btnDown.addEventListener('click', () => movePlayer(0, 1));
+
+function getPositionInCanvas(col, row) {
+	return {
+		x:
+			// Initial x-position to render an element
+			elementSize * col +
+			// Space between columns
+			col * canvasSize * 0.02,
+
+		y:
+			// Initial y-position to render an element
+			elementSize * (row + 1) +
+			// Space between rows
+			(row + 1) * canvasSize * 0.02 -
+			// Space between canvas top and elements
+			canvasSize * 0.018,
+	};
+}
+
+function getMap(level) {
+	// Get the map corresponding to the level
+	return (
+		maps[level - 1]
+			// Remove spaces
+			.trim()
+			// Get rows with elements
+			.split('\n')
+			// Remove spaces and get elements individually
+			.map((row) => row.trim().split(''))
+	);
+}
+
+/**
+ * Move the player horizontally and vertically.
+ * @param {Number} cols Number of columns to move.
+ * If negative it moves to the left, otherwise, it moves to the right.
+ * @param {Number} rows Number of rows to move.
+ * If negative it moves down, otherwise, it moves up.
+ */
+function movePlayer(cols, rows) {
+	playerPos.col += cols;
+	playerPos.row += rows;
+
+	const posInCanvas = getPositionInCanvas(playerPos.col, playerPos.row);
+	const newX = posInCanvas.x;
+	const newY = posInCanvas.y;
+
+	game.fillText(emojis['P'], newX, newY);
 }
 
 function movePlayerByKey(keyCode) {
-	if (keyCode == 'KeyW' || keyCode == 'ArrowUp') movePlayer(0, 1);
+	if (keyCode == 'KeyW' || keyCode == 'ArrowUp') movePlayer(0, -1);
 	else if (keyCode == 'KeyA' || keyCode == 'ArrowLeft') movePlayer(-1, 0);
 	else if (keyCode == 'KeyD' || keyCode == 'ArrowRight') movePlayer(1, 0);
-	else if (keyCode == 'KeyS' || keyCode == 'ArrowDown') movePlayer(0, -1);
+	else if (keyCode == 'KeyS' || keyCode == 'ArrowDown') movePlayer(0, 1);
 }
 
 function renderMap(level) {
-	// Get the map corresponding to the level
-	const map = maps[level - 1]
-		// Remove spaces
-		.trim()
-		// Get each row of elements
-		.split('\n')
-		// Remove spaces and get elements
-		.map((row) => row.trim().split(''));
+	const map = getMap(level);
+
+	let posInCanvas = undefined;
+	let x = undefined;
+	let y = undefined;
 
 	map.forEach((arr, row) => {
 		arr.forEach((element, col) => {
-			game.fillText(
-				emojis[element],
-				// Initial x-position to render an element
-				elementSize * col +
-					// Space between columns
-					col * canvasSize * 0.02,
+			posInCanvas = getPositionInCanvas(col, row);
+			x = posInCanvas.x;
+			y = posInCanvas.y;
 
-				// Initial y-position to render an element
-				elementSize * (row + 1) +
-					// Space between rows
-					(row + 1) * canvasSize * 0.02 -
-					// Space between canvas top and elements
-					canvasSize * 0.018
-			);
+			game.fillText(emojis[element], x, y);
+
+			if (element == 'P') {
+				playerPos.col = col;
+				playerPos.row = row;
+			}
 		});
 	});
 }
@@ -92,5 +134,5 @@ function startGame() {
 	resizeAll();
 	renderMap(currentLevel);
 
-	console.log({ canvasSize, elementSize });
+	console.log(playerPos);
 }
