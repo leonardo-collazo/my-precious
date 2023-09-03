c8b1220504b407b7b198cc6c4a28aaaa0debfdd0
KidBourbon <kidbourbongamer@gmail.com>
2023-09-03 17:38:49 -0400

    More levels were added until it has 10 levels. Asigned a different emoji set to each level

--- js/maps.js
+++ js/maps.js
@@ -4,11 +4,6 @@
  */
 
 const emojis = {
-	'-': ' ',
-	P: '🐺',
-	G: '🍖',
-	D: '💣',
-	X: '🌳',
 	BOMB_COLLISION: '🔥',
 	GAME_OVER: '😞',
 	WIN: '😋',
@@ -17,6 +12,79 @@ const emojis = {
 	TIME: '⏱',
 	RECORD: '🏁',
 };
+
+const emojisPerLevel = [
+	{
+		'-': ' ',
+		P: '🐺',
+		G: '🍖',
+		X: '🌲',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '🤠',
+		G: '🐎',
+		X: '🌵',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '🧛🏻‍♂️',
+		G: '🩸',
+		X: '💀',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '🧙🏻‍♂️',
+		G: '🧪',
+		X: '🎃',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '👨🏻‍💻',
+		G: '🎮',
+		X: '🌴',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '👨🏻',
+		G: '🍄',
+		X: '🧱',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '🧝🏻‍♂️',
+		G: '🏹',
+		X: '🌳',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '🤴🏻',
+		G: '👸🏻',
+		X: '🗻',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '🧟‍♂️',
+		G: '🧠',
+		X: '🏠',
+		D: '💣',
+	},
+	{
+		'-': ' ',
+		P: '👨🏻‍🦱',
+		G: '💍',
+		X: '🌋',
+		D: '💣',
+	},
+];
 
 const maps = [];
 maps.push(`
@@ -40,11 +108,11 @@ maps.push(`
   X-XXXD-XXX
   XD--XD--DX
   XX--XXD-DX
-  XXXX---GXX
+  XXXD---GXX
   XXXXXXXXXX
   `);
 maps.push(`
-  G-----DXXX
+  XG----DXXX
   XXDXX-DXXX
   XD----DXXX
   XD-XXXDXXX
@@ -53,5 +121,89 @@ maps.push(`
   XD-----DXX
   XD-XXXXXXX
   XD-----PXX
+  XXXXXXXXXX
+`);
+maps.push(`
+  -PXXXXXXXX
+  -DXXXXXXXX
+  --DXXXXXXX
+  D--DXXXXXX
+  XD-XXXXXXX
+  XX--DXXXXX
+  XXD-XXGXXX
+  XXX-XD-DXX
+  X---DD---X
+  XXD----DXX
+`);
+maps.push(`
+  X-----XXXX
+  XXXXD----D
+  ------DD--
+  -DXXXDXXD-
+  -XD-------
+  -XX-DX-XXX
+  --D-XXPXXX
+  D-X-XXXXXX
+  D-X-XXXXXX
+  XGXXXXXXXX
+`);
+maps.push(`
+  G-DXXXXXXX
+  D--DXXXXXX
+  XD--DXXXXX
+  XXD--DXXXX
+  XXXD--DXXX
+  XXXXD--DXX
+  XXXXXD--DX
+  XXXXXXD--D
+  XXXXXXXD-D
+  XP-------D
+`);
+maps.push(`
+  PXG-------
+  -XXXXXXXD-
+  ----DD----
+  DXX-XXXXD-
+  ----DXDXX-
+  -DX-XX-XX-
+  DXX-XX-XX-
+  ----XX-XD-
+  XXD-DX-D--
+  XXX------D
+`);
+maps.push(`
+  XXP------D
+  XXDXXXXD-D
+  XX-----D-X
+  XX-DDD-X-X
+  XX-DGD-X-X
+  XX-D-D-X-X
+  XX-X---X-X
+  XX-DXXDD-X
+  XD-------D
+  XXDXXXXXDX
+`);
+maps.push(`
+  D---------
+  -XXD-DXXD-
+  -XXX-XXXX-
+  -XXX-XXXX-
+  ----P---D-
+  -XXX-XXXX-
+  -XXX-XXXX-
+  -XXX-XXXD-
+  -XGDXXD---
+  DD------DD
+`);
+maps.push(`
+  XD-----DXX
+  XD-DXD--GX
+  X--XX--DXX
+  X-DXX-XX-X
+  X-XXX-XX-X
+  X-DXX-XD-X
+  D--XD----X
+  XD-DXXXD-X
+  XXP------D
   XXXXXXXXXX
 `);
